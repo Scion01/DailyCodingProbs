00469575e1f08c18678c66e73aa32fff7ce306f5
scion01 <nishantnareshtyagi@gmail.com>
2019-08-16 09:32:01 +0530

    minor change

---
+++ sorted_square.cpp
@@ -0,0 +1,21 @@
+/*Given a sorted list of integers, square the elements and give the output in sorted order.*/
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;++i){
+		cin>>a[i];
+		a[i]*=a[i];
+	}
+	sort(a,a+n);
+	for(int i=0;i<n;++i)
+		cout<<a[i]<<endl;
+
+	return 0;
+}
