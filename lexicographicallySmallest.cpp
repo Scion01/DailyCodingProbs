cbe14dfc82988d6ed5e2b7dd8ed7b5b964804033
scion01 <nishantnareshtyagi@gmail.com>
2020-03-30 22:39:56 +0530

    new problem description added

--- lexicographicallySmallest.cpp
+++ lexicographicallySmallest.cpp
@@ -1,3 +1,7 @@
+/*Given a string S consisting of lowercase alphabets. The task is to find the lexicographically smallest string X of the same length only that can be formed using the operation given below:
+
+In a single operation, select any one character among the at most first K characters of string S, remove it from string S and append it to string X. Apply this operation as many times as he wants.*/
+
 #include<iostream>
 #include<bits/stdc++.h>
 #include<cstring>
